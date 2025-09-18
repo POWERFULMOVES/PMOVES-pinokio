8e634322c217ed558d4537b1439a9235677f5992
Barathicite <112423465+Barathicite@users.noreply.github.com>
2025-09-18 19:48:50 -0400

    feat(gateway-demo): wire mindmap router

--- pmoves_gateway_demo/gateway/main.py
+++ pmoves_gateway_demo/gateway/main.py
@@ -6,6 +6,7 @@ from fastapi.responses import FileResponse, HTMLResponse
 from gateway.api.chit import router as chit_router
 from gateway.api.signaling import router as sig_router
 from gateway.api.viz import router as viz_router
+from gateway.api.mindmap import router as mindmap_router
 from gateway.api.workflow import router as workflow_router
 from gateway.api.events import router as events_router
 
@@ -21,6 +22,7 @@ app.include_router(sig_router)
 app.include_router(viz_router)
 app.include_router(workflow_router)
 app.include_router(events_router)
+app.include_router(mindmap_router)
 app.mount("/web", StaticFiles(directory="web"), name="web")
 # Expose generated outputs as read-only static routes
 app.mount("/data", StaticFiles(directory="data", check_dir=False), name="data")
